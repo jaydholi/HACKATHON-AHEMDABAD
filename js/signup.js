ab9fbe0e8ffb9801b151b25dc44cf1fbb8080a38
Joshi Bhavya <160045396+bhavya-joshi0098@users.noreply.github.com>
2025-01-22 12:57:12 +0530

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,10 +1,16 @@
 document.getElementById('signup-form').addEventListener('submit', function(event) {
   event.preventDefault();
   
+  const firstName = document.getElementById('first-name').value;
+  const lastName = document.getElementById('last-name').value;
+  const phone = document.getElementById('phone').value;
   const email = document.getElementById('email-signup').value;
   const password = document.getElementById('password-signup').value;
 
-  if (email && password) {
+  if (firstName && lastName && phone && email && password) {
+      localStorage.setItem('userFirstName', firstName);
+      localStorage.setItem('userLastName', lastName);
+      localStorage.setItem('userPhone', phone);
       localStorage.setItem('userEmail', email);
       localStorage.setItem('userPassword', password);
       alert('Account created successfully!');
