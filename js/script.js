3d360f040d7f7f526f59c1710bfc2ebf10dac34c
bhavya joshi <bhavyajoshi324@gmail.com>
2025-01-24 11:55:47 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -6,3 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
         welcomeSpan.textContent = userFirstName;
     }
 });
+
+//  diable login and sign-up button if user is logged in 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const welcomeSpan = document.getElementById('Welcome_span');
+    const loginButton = document.querySelector('a[href="/pages/login.html"]').parentElement;
+    const signUpButton = document.querySelector('a[href="/pages/sign-up.html"]').parentElement;
+
+    if (welcomeSpan.textContent.trim() !== 'GUEST') {
+        loginButton.style.display = 'none';
+        signUpButton.style.display = 'none';
+    }
+});
