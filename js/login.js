8b98d61fb074d9c1a72cb84d6cc160562f3f42f4
bhavya joshi <bhavyajoshi324@gmail.com>
2025-01-24 12:44:45 +0530

    commit

--- js/login.js
+++ js/login.js
@@ -7,7 +7,13 @@ document.getElementById('login-form').addEventListener('submit', function(event)
   const storedEmail = localStorage.getItem('userEmail');
   const storedPassword = localStorage.getItem('userPassword');
 
-  if (email === storedEmail && password === storedPassword) {
+  const Admin_email = "Admingov_mail@outlook.com";
+  const Admin_password = "Admin123";
+  if (email === Admin_email && password === Admin_password) {
+      alert('Login successful!');
+      window.location.href = '../pages/officerdashboard.html';
+  }
+  else if (email === storedEmail && password === storedPassword) {
       alert('Login successful!');
       window.location.href = 'citizen.html';
   } else {
